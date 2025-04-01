e1bc5fc706c1d40f33306ef356e3f5a5018c27fa
Shreshth Paliyal <paliyalrainbow@gmail.com>
2025-04-01 16:42:01 +0530

    POC for circuitBreaker service

---
+++ customCB/src/main/java/com/practice/customCB/CustomCircuitBreakerApplication.java
@@ -0,0 +1,13 @@
+package com.practice.customCB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomCircuitBreakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomCircuitBreakerApplication.class, args);
+	}
+
+}
